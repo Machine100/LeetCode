53f72389b014e614aeffb0986147dcf81424aa98
Machine100 <joesmileyyy@yahoo.com>
2020-01-10 08:13:32 -0500

    .

---
+++ typethings.ts
@@ -0,0 +1,13 @@
+// given an array, do a bubble sort.
+
+let sortMe:number[] = []
+
+
+for (let i=0; i<100; i++) {
+    let randomNumber = Math.floor(Math.random() * 100 + 1)
+    sortMe[i] = randomNumber
+}
+console.log(sortMe)
+
+
+
