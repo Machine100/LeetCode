ab9412312a7917af900500126beebc43a8f22250
Machine100 <joesmileyyy@yahoo.com>
2020-01-17 15:40:48 -0500

    .

---
+++ Bubblesort.ts
@@ -0,0 +1,27 @@
+// given an array, do a bubble sort.
+
+let sortMe:number[] = []
+
+
+for (let i=0; i<100; i++) {
+    let randomNumber = Math.floor(Math.random() * 100 + 1)
+    sortMe[i] = randomNumber
+}
+console.log(sortMe)
+console.log(sortMe.length)
+
+sortMe.forEach( ()=>{
+    for(let i=0; i<=sortMe.length-1; i++) {
+        if (sortMe[i]>sortMe[i+1]) {
+            let temp:number = sortMe[i]
+            sortMe[i] = sortMe[i+1]
+            sortMe[i+1] = temp
+            console.log('swapped')
+        }
+    }
+})
+
+console.log(sortMe)
+
+
+
