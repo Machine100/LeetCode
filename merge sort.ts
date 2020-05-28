cedc626ca0604736b6113dc94075333c93df5c96
Machine100 <joesmileyyy@yahoo.com>
2020-05-28 14:58:43 -0400

    added code, not complete yet

--- merge sort.ts
+++ merge sort.ts
@@ -14,15 +14,26 @@ const sortMa: number[] = [4,6,7,8,2,3,4,8,6,2,5,7,9,0,7,6,4,3,6,7,8,6,5,43,6,8,8
 //   iterate over to combine 
 //                           --return single array that is completly sorted
 
-let mergeSort = (referenceTosortMA: number[]) => {
-    let pointerLeft: number = 0
-    let pointerRight: number = Math.floor(sortMa.length/2)
-    if (pointerLeft === pointerRight - 1) {
-        combine(referenceTosortMA, pointerLeft, pointerRight); return}   // check for base case of pointers adjacent
+let mergeSort = (referenceTosortMA: number[], pointerStart?: number, pointerEnd?:number) => {
+    if (!pointerStart) pointerStart = 0
+    if (!pointerEnd) pointerEnd = Math.floor(sortMa.length/2)
+    if (pointerStart === pointerEnd - 1) {             // check for base case of pointers adjacent
+        combine(referenceTosortMA, pointerStart, pointerEnd);
+        return
+    }
+    let pointerMiddle: number = Math.floor( (pointerStart + pointerEnd)/2 )
+    mergeSort(sortMa, pointerStart, pointerMiddle-1)      // pass left side
+    mergeSort(sortMa, pointerMiddle, pointerEnd)
+    combine (sortMa, pointerStart, pointerMiddle)
+    return
 }
 
-let combine = (referenceTosortMa: number[], pointerLeft: number, pointerRight: number) => {
-    if (pointerLeft === pointerRight) { return }
-
+let combine = (referenceTosortMa: number[], leftStart: number, rightEnd: number) => {
+    if (leftStart === rightEnd) return
+    let rightStart: number = Math.floor( (leftStart + rightEnd) / 2 )
+    let leftEnd: number = rightStart - 1
+    for (let i=0; i <= (rightEnd - leftStart); i++) {
+        
+    }
     return   // don't have to return andy value because function is working on the reference
 }
