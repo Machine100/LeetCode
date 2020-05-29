40caf3e04b954b617ce306e2643d13a59d5d932a
Machine100 <joesmileyyy@yahoo.com>
2020-05-29 09:48:50 -0400

    working up algo

--- merge sort.ts
+++ merge sort.ts
@@ -1,27 +1,13 @@
 const sortMa: number[] = [4,6,7,8,2,3,4,8,6,2,5,7,9,0,7,6,4,3,6,7,8,6,5,43,6,8,8]
 
-// magicSort(array)
-//   chk if array cannot be split (pointers are adj)           -- chk for base contition first so that can escape loop
-//     if so                                                   -- and then escape
-//        call combine(array, pointerleft, pointerright)
-//        return
-//     otherwise                                               -- do the recursion
-//       magicsort(leftArray)
-//       magicsort(rightArray)
-//       combine(array, pointerleft, pointerright)             -- call base condition
-//
-// combine(array,lft,rght)   --recieve 2 arrays that were presorted internally
-//   iterate over to combine 
-//                           --return single array that is completly sorted
-
 let mergeSort = (referenceTosortMA: number[], pointerStart?: number, pointerEnd?:number) => {
-    if (!pointerStart) pointerStart = 0
-    if (!pointerEnd) pointerEnd = Math.floor(sortMa.length/2)
+    if (pointerStart === null) pointerStart = 0
+    if (pointerEnd === null) pointerEnd = Math.ceil(sortMa.length/2)
     if (pointerStart === pointerEnd - 1) {             // check for base case of pointers adjacent
         combine(referenceTosortMA, pointerStart, pointerEnd);
         return
     }
-    let pointerMiddle: number = Math.floor( (pointerStart + pointerEnd)/2 )
+    let pointerMiddle: number = Math.ceil( (pointerStart + pointerEnd)/2 )
     mergeSort(sortMa, pointerStart, pointerMiddle-1)      // pass left side
     mergeSort(sortMa, pointerMiddle, pointerEnd)
     combine (sortMa, pointerStart, pointerMiddle)
@@ -30,10 +16,31 @@ let mergeSort = (referenceTosortMA: number[], pointerStart?: number, pointerEnd?
 
 let combine = (referenceTosortMa: number[], leftStart: number, rightEnd: number) => {
     if (leftStart === rightEnd) return
-    let rightStart: number = Math.floor( (leftStart + rightEnd) / 2 )
+
+    let rightStart: number = Math.ceil( (leftStart + rightEnd) / 2 )
     let leftEnd: number = rightStart - 1
-    for (let i=0; i <= (rightEnd - leftStart); i++) {
-        
+
+    let leftIndex:number = leftStart
+    let rightIndex:number = rightStart
+
+    let tempMa: number[] = [] 
+    while (leftIndex <= leftEnd && rightIndex <= rightEnd) {
+        if (sortMa[leftIndex] <= sortMa[rightIndex]) {
+            tempMa.push(sortMa[leftIndex])
+            leftIndex++
+        } else {
+            tempMa.push(sortMa[rightIndex])
+            rightIndex++
+        }
     }
-    return   // don't have to return andy value because function is working on the reference
+    while (leftIndex <= leftEnd) {    // if anything left on left side push them all onto end of stack
+        tempMa.push(sortMa[leftIndex])
+        leftIndex++
+    }
+    while (rightIndex <= rightEnd) {
+        tempMa.push(sortMa[rightIndex])
+        rightIndex++
+    }
+    
+    return
 }
