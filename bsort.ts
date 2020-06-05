aac503e869410393ce9d4e16ed67a2659e2e800d
Machine100 <joesmileyyy@yahoo.com>
2020-06-05 10:36:45 -0400

    .

---
+++ bsort.ts
@@ -0,0 +1,14 @@
+// Create a basic bubblesort algo. But rules:
+//  accurate keystrokes
+//  work in all possible vim keys
+//  for arrow function: implement it so that it's xyz.sort() instead of sort(xyz) and then do xyz.sort().tocaps
+
+import { number } from "prop-types"
+
+let srtMe: number[] = [4,6,7,8,45,3,2,4,5,654,455,34,56,32,456,24,563455,45,6,43,562,45,5,5,547,7,8,3,232,3,3]
+
+let sort = (thing: number[]) => {
+    
+}
+
+number.sort()
