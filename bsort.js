16f19a30b818a195bf69b63ed81235a77a3211dc
Machine100 <joesmileyyy@yahoo.com>
2020-06-09 09:37:59 -0400

    .

--- bsort.js
+++ bsort.js
@@ -5,10 +5,22 @@
 //  for arrow function: implement it so that it's xyz.sort() instead of sort(xyz) and then do xyz.sort().tocaps
 exports.__esModule = true;
 var srtMe = [4, 6, 7, 8, 45, 3, 2, 4, 5, 654, 455, 34, 56, 32, 456, 24, 563455, 45, 6, 43, 562, 45, 5, 5, 547, 7, 8, 3, 232, 3, 3];
-var sort = function (thing) {
+var sort = function () {
+    for (var i = 0; i <= srtMe.length; i++) {
+        for (var j = 0; j <= srtMe.length - 1 - i; j++) {
+            console.log('JI:', j, i);
+            if (srtMe[j] < srtMe[j + 1]) {
+                var temp = srtMe[j];
+                srtMe[j] = srtMe[j + 1];
+                srtMe[j + 1] = temp; // swap the values
+                console.log(i, j);
+            }
+        }
+    }
 };
 var applyToAll = function () {
     var result = srtMe.reduce(function (accumulatedSum, currentValue) { return accumulatedSum + currentValue; });
     console.log(result);
 };
-applyToAll();
+sort();
+console.log(srtMe);
--- bsort.ts
+++ bsort.ts
@@ -7,11 +7,22 @@ import { number } from "prop-types"
 
 let srtMe: number[] = [4,6,7,8,45,3,2,4,5,654,455,34,56,32,456,24,563455,45,6,43,562,45,5,5,547,7,8,3,232,3,3]
 
-let sort = (thing: number[]) => {
+let sort = () => {
+    for (let i=0; i<=srtMe.length; i++) {
+        for (let j=0; j<=srtMe.length-1-i; j++) {
+            console.log('JI:', j, i)
+            if (srtMe[j]<srtMe[j+1]) {
+                let temp = srtMe[j]; srtMe[j]=srtMe[j+1]; srtMe[j+1]=temp   // swap the values
+                console.log(i,j)
+            }
+        }
+    }
 }
+
 let applyToAll = () => {
     const result = srtMe.reduce((accumulatedSum, currentValue) => accumulatedSum + currentValue)
     console.log(result)
 }
 
-applyToAll()
+sort()
+console.log(srtMe)
